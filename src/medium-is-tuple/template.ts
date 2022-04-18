1c4e609a9f2d649c52023a20b35515538bdce4d9
white-letter <121116111@qq.com>
2022-04-19 00:07:25 +0800

    doc: add comment

--- src/medium-is-tuple/template.ts
+++ src/medium-is-tuple/template.ts
@@ -1,5 +1,8 @@
-// type AAA = number extends 2 ? true :false
+// type AAA = number extends 2 ? true : false;
+// type CCC = string[]['length']
 
+// 1.首先判断数组结构
+// 2.排除是数组  数组['length']返回的是number
 type IsTuple<T> = T extends readonly unknown[]
   ? number extends T["length"]
     ? false
